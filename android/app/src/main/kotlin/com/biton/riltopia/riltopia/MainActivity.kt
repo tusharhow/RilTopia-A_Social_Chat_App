9ee49030d5daf956d6d9c7810b0410c5f20fbd57
Idan054 <idanbit80@gmail.com>
2022-02-22 16:37:40 +0200

    V1.0 - Build design

---
+++ android/app/src/main/kotlin/com/biton/riltopia/riltopia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.biton.riltopia.riltopia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/theme/themes.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ColorScheme kColorScheme = const ColorScheme(
+  brightness: Brightness.light,
+  primaryVariant: cGrey500,     // 500
+  primary: cRilPurple,          // 450 --
+  onPrimary: cRilPurple,        // 400 --
+  secondaryVariant: cRilPurple, // 350 --
+  secondary: cGrey300,          // 300
+  onSecondary: cRilPurple,      // 250 --
+  surface: cRilPurple,          // 200 --
+  onSurface: cGrey150,          // 150
+  background: cGrey100,         // 100
+  onBackground: cGrey50,        // 50
+  error: kErrorRed,
+  onError: kColorSpiderRed,
+);
+
+ThemeData buildLightTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    splashColor: cRilPurple,
+    hintColor: cRilSecondaryPurple,
+    canvasColor: cRilSurfacePurple,
+    primaryColorDark: cLightPurple,
+    primaryColorLight: cLightPurple,
+    colorScheme: kColorScheme,
+    primaryColor: primaryColor, // Colors.black,
+    backgroundColor: backgroundColor, // Colors.white,
+    scaffoldBackgroundColor: scaffoldBackground,
+  );
+}
+
+ // A perfect opposite of  kColorScheme
+ColorScheme kColorSchemeDark = const ColorScheme(
+  brightness: Brightness.light,
+  primaryVariant: cGreyDark500,     // 500
+  primary: cRilPurple,              // 450 --
+  onPrimary: cRilPurple,            // 400 --
+  secondaryVariant: cRilPurple,     // 350 --
+  secondary: cGreyDark300,          // 300
+  onSecondary: cRilPurple,          // 250 --
+  surface: cRilPurple,              // 200 --
+  onSurface: cGreyDark150,          // 150
+  background: cGreyDark100,         // 100
+  onBackground: cGreyDark50,        // 50
+  error: kErrorRed,
+  onError: kColorSpiderRed,
+);
+
+ThemeData buildDarkTheme() {
+  final ThemeData base = ThemeData.dark();
+  return base.copyWith(
+    splashColor: cRilPurple,
+    hintColor: cRilSecondaryPurple,
+    canvasColor: cRilSurfacePurple,
+    primaryColorDark: cLightPurple,
+    primaryColorLight: cLightPurple,
+    colorScheme: kColorScheme,  // Todo
+    primaryColor: backgroundColor, // Colors.white,
+    backgroundColor: primaryColor, // Colors.black,
+    scaffoldBackgroundColor: scaffoldDarkBackground,
+  );
+}
